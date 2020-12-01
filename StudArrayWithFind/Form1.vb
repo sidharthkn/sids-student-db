adbc5332bb4bd06de7f34a32ac2cdb415c1b5fc6
Sidharth Kannegal Narayanan <sidharth.kn@gmail.com>
2020-12-01 17:26:56 +1100

    v1.2 - Gender Validation

--- StudArrayWithFind/Form1.vb
+++ StudArrayWithFind/Form1.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form1
-    'set up a record or "class" for a student
+    'set up a record or "class" for a student.
     Class STUDENT
         Public firstname As String
         Public lastname As String
